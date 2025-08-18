9bbcbe61f653e6c4de4e85afd85b9463775a269e
Prachetas Shukla <91451358+ThePrachiShuk@users.noreply.github.com>
2025-08-18 10:46:00 +0530

    Added Links to app.py

--- app.py
+++ app.py
@@ -746,8 +746,8 @@ app.layout = html.Div(id="theme-root", className="theme-dark perf-smooth", child
                     html.Span(" | Built with ❤️ using Dash & Python", className="app-version")
                 ]),
                 html.Div([
-                    html.A("Documentation", href="#", className="me-3"),
-                    html.A("GitHub", href="#", className="me-3"),
+                    html.A("Documentation", href="https://github.com/ThePrachiShuk/EcoFuelFusion/blob/main/README.md", className="me-3"),
+                    html.A("GitHub", href="https://github.com/ThePrachiShuk/EcoFuelFusion", className="me-3"),
                     html.A("Support", href="#"),
                 ], className="footer-links")
             ], className="footer-content")
@@ -1369,4 +1369,5 @@ def route_from_landing(n_clicks, active):
 
 # ========== RUN APPLICATION ==========
 if __name__ == "__main__":
+
     app.run(debug=True, port=8050)
